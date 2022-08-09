a98e2b75f27ca89f9946bcb2d32f3ae5b090ff2a
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-09 22:36:03 +0800

    zxsc

---
+++ src/main/java/DeadlockMain.java
@@ -0,0 +1,62 @@
+public class DeadlockMain {
+
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+
+        new Thread1().start();
+        new Thread2().start();
+
+    }
+
+    private static class Thread1 extends Thread {
+        public void run()
+        {
+            synchronized(DeadlockMain.lock1)
+            {
+                System.out.println("Thread 1 : has lock 1");
+                try{
+                    Thread.sleep(100);
+                }catch (InterruptedException e) {
+
+                }
+
+                System.out.println("thread 1 : waitingfor lock 2");
+
+                synchronized(lock2)
+                {
+                    System.out.println("Thread 1 : has lock 1 and lock 2");
+                }
+                System.out.println("Thread 1: released lock 2 ");
+            }
+            System.out.println("Thread 1: released lock 1 . exiting");
+        }
+    }
+
+    private static class Thread2 extends Thread {
+        public void run() {
+            synchronized(lock2)
+            {
+                System.out.println("thread 2 has lock 2");
+                try{
+                     Thread.sleep(100);
+                }catch (InterruptedException e)
+                {
+
+                }
+
+                System.out.println("Thread 2 waiting for lock 1");
+
+                synchronized(lock1)
+                {
+                    System.out.println("Thread 2: has lock 2 and lock 1");
+                }
+                System.out.println("Thread 2 released lock 1");
+            }
+            System.out.println("Thread 2 released lock 2 . exiting...");
+        }
+    }
+}
+
+
