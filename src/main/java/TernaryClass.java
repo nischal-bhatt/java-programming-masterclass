92e4601a0406fb41b1eb93ff266c666c88964ec4
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-09 19:55:58 +0800

    asdaf

---
+++ src/main/java/TernaryClass.java
@@ -0,0 +1,12 @@
+public class TernaryClass {
+
+    public static void main(String[] args) {
+
+        boolean val = false;
+
+        String x = val ? "hellow" : "World";
+
+        System.out.println(x);
+
+    }
+}
