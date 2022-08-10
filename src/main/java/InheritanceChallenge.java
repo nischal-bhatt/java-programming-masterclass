b35cd34ffca3038ca1fe60b437803ab5998fe5fb
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-10 23:31:17 +0800

    inheritance challenge

---
+++ src/main/java/InheritanceChallenge.java
@@ -0,0 +1,13 @@
+public class InheritanceChallenge {
+    public static void main(String[] args) {
+
+        Hyundai h = new Hyundai(36);
+
+        h.steering(45);
+        h.accelerate(30);
+        h.accelerate(20);
+        h.accelerate(-42);
+    }
+
+
+}
