4a086ce2b4213721fee1a2d51811952718320205
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-11 09:42:06 +0800

    d

---
+++ src/main/java/ImmutableClass.java
@@ -0,0 +1,13 @@
+public class ImmutableClass {
+    private int dataField;
+
+    public ImmutableClass(int dataField)
+    {
+        this.dataField = dataField;
+    }
+
+    public int getDataField()
+    {
+        return this.dataField;
+    }
+}
---
+++ src/main/java/ImmutableObjectAndClass.java
@@ -0,0 +1,9 @@
+public class ImmutableObjectAndClass {
+    public static void main(String[] args) {
+        //string class is immutable class
+
+
+        ImmutableClass a = new ImmutableClass(3);
+        System.out.println(a.getDataField());
+    }
+}
