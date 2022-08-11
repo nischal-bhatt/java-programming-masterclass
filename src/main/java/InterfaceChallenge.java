a3fc1576a56f828fb23afa3f1fa0d1d807ec58fc
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-11 09:21:26 +0800

    ygh

---
+++ src/main/java/InterfaceChallenge.java
@@ -0,0 +1,23 @@
+public class InterfaceChallenge {
+    public static void main(String[] args) {
+
+        GearBox mcLaren = new GearBox(6);
+
+
+
+        mcLaren.operateClutch(true);
+
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
+
+
+    }
+
+}
